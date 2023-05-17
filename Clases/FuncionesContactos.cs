531e500600587fd1d3ebffa0d6fa41651f9b1343
LuisGalindez <125323011+LuisGalindez@users.noreply.github.com>
2023-05-17 16:36:08 -0400

    lista de contactos

--- Clases/FuncionesContactos.cs
+++ Clases/FuncionesContactos.cs
@@ -37,13 +37,20 @@ namespace Prueba_Maui.Clases
             //_listaDeContactosOriginal.Clear();
             //Al primer contacto se le debe pasar como parametro un string vacio
             _listaDeContactosOriginal.Add(new Contacto("Luis","Galindez","Editor","a@gmail.com","042425836912",_listaDeContactosOriginal.Count+1,""));
-
+            _listaDeContactosOriginal.Add(new Contacto("Eugenio", "Montilla", " ", "a@gmail.com", "041257632497", _listaDeContactosOriginal.Count + 1, ""));
+            _listaDeContactosOriginal.Add(new Contacto("Brian", "Marquez", " ", "a@gmail.com", "042643268451", _listaDeContactosOriginal.Count + 1, ""));
+            _listaDeContactosOriginal.Add(new Contacto("Alejandro", "Ramirez", "Carpintero", "a@gmail.com", "04242425403", _listaDeContactosOriginal.Count + 1, ""));
+            _listaDeContactosOriginal.Add(new Contacto("Francisco", "Ochoca", "El Lider", "a@gmail.com", "04165785546", _listaDeContactosOriginal.Count + 1, ""));
+            _listaDeContactosOriginal.Add(new Contacto("Maria", "Soto", " ", "a@gmail.com", "04126294867", _listaDeContactosOriginal.Count + 1, ""));
+            _listaDeContactosOriginal.Add(new Contacto("Alex", "Romero", " ", "a@gmail.com", "042651684165", _listaDeContactosOriginal.Count + 1, ""));
+            _listaDeContactosOriginal.Add(new Contacto("Gipsander", " ", " ", "a@gmail.com", "04148926340", _listaDeContactosOriginal.Count + 1, ""));
+            _listaDeContactosOriginal.Add(new Contacto("Maria", "Castillo", "Uñas", "a@gmail.com", "042665426515", _listaDeContactosOriginal.Count + 1, ""));
             /*for(int i=0; i<5 ; i++)
             {
                 _listaDeContactosOriginal.Add(new Contacto("Chupa", "Paletas", "ApodoXd", "b@gmail.com", 36656, _listaDeContactosOriginal.Count + 1, _listaDeContactosOriginal[_listaDeContactosOriginal.Count - 1].PathImagen));
                 _listaDeContactosOriginal.Add(new Contacto("Super", "Man", "SuperMan", "c@gmail.com", 75862, _listaDeContactosOriginal.Count + 1, _listaDeContactosOriginal[_listaDeContactosOriginal.Count - 1].PathImagen));
                 _listaDeContactosOriginal.Add(new Contacto("Luis", "Galindez", "Editor", "a@gmail.com", 24456, _listaDeContactosOriginal.Count + 1, _listaDeContactosOriginal[_listaDeContactosOriginal.Count - 1].PathImagen));
-            }  */ 
+            }  */
         }
 
         //Funciones Normales
